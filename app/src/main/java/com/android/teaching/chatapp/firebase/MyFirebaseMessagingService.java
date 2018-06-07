ae7ae15035e7e06312fc9e35139d34acd197fcc7
Juan Antonio Durán Ramírez <31082015+juanantoniomkt@users.noreply.github.com>
2018-06-07 13:36:47 +0200

    última prueba notificación

--- app/src/main/java/com/android/teaching/chatapp/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/android/teaching/chatapp/firebase/MyFirebaseMessagingService.java
@@ -1,15 +1,7 @@
 package com.android.teaching.chatapp.firebase;
 
-import android.app.NotificationManager;
-import android.content.Context;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-import java.util.Map;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -17,8 +9,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
         super.onMessageReceived(remoteMessage);
-        Map<String, String> data = remoteMessage.getData();
-
     }
 
 }
